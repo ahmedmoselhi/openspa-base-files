118519e31c24e851321afcd66d9b041e259fb9db
ahmedmoselhi55 <ahmedmoselhi55@gmail.com>
2020-09-05 02:30:57 +0200

    add plug load

--- files/usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/__init__.py
+++ files/usr/lib/enigma2/python/Plugins/SystemPlugins/DeviceManager/__init__.py
@@ -1,17 +1,16 @@
+from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 import gettext
 
 def localeInit():
-    gettext.bindtextdomain('DeviceManager', resolveFilename(SCOPE_PLUGINS, 'SystemPlugins/DeviceManager/locale'))
-
+	gettext.bindtextdomain("DeviceManager", resolveFilename(SCOPE_PLUGINS, "SystemPlugins/DeviceManager/locale"))
 
 def _(txt):
-    t = gettext.dgettext('DeviceManager', txt)
-    if t == txt:
-        print '[DeviceManager] fallback to default translation for:', txt
-        t = gettext.gettext(txt)
-    return t
-
+	t = gettext.dgettext("DeviceManager", txt)
+	if t == txt:
+		#print "[DeviceManager] fallback to default translation for:", txt
+		t = gettext.gettext(txt)
+	return t
 
 localeInit()
 language.addCallback(localeInit)
